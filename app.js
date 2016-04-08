ebc0fd46251802af766a6f6349830a6c687916db
lisssb <liss.sb@gmail.com>
2016-04-08 19:53:45 +0200

    second

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 
-var app = angular.module("diaryApp", []);
+var app = angular.module('app', []);
 
-app.controller('diaryCtrl', [function(){
+app.controller('ParentCtrl', [function(){
   this.title="Mi agenda";
+  this.saluda = function(user){
+    alert(user.name);
+  };
+  this.onClick = function(e){
+    //console.log(e)
+    alert("Hola lola: " + e);
+  }
+  this.onChange= function(){
+    alert("cambio");
+  };
+
+  this.users = [{
+    name : 'Liss',
+    age : 28
+  }, {
+    name : 'Marcial',
+    age : 27
+  },
+  {
+    name : "Elba",
+    age : 45
+  }];
+
+
+  
+}]);
+
+app.controller('ChildCtrl', [function(){
+  this.title="Controlador hijo";
+  //var a = $scope;
+  //debugger;
 }]);
